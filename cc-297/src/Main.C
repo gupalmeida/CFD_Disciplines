572a1d8a1d2c1a6e32553fbd906375e5031cdc37
gupalmeida <gupalmeida@gmail.com>
2022-05-28 00:10:03 -0300

    Fixed CMake structure. Code now compiles and LinearAlgebra, ODESolver and IOobjects are now treated as libraries.

--- cc-297/src/Main.C
+++ cc-297/src/Main.C
@@ -3,9 +3,9 @@
 
 #include "Matrix.H"
 #include "Vector.H"
-#include "SecondOrderODE.H"
-#include "LinSys.H"
-#include "IOobject.H"
+// #include "SecondOrderODE.H"
+// #include "LinSys.H"
+// #include "IOobject.H"
 
 int main()
 {
@@ -18,26 +18,8 @@ int main()
     m.setUpperDiag( 1.0 );
     m.setLowerDiag( 1.0 );
     m.setDiag( -2.0 );
-
-    LinSys s(m,b);
-    Vector sol(5);
-
-    // measuring time
-    auto start = chrono::steady_clock::now();
-
-    sol = s.Solve();
-
-    print( sol );
-
-    auto end = chrono::steady_clock::now();
-    cout
-        << "Elapsed Solver Time [s]: "
-        << chrono::duration_cast<chrono::nanoseconds>(end - start).count()
-        << "\n";
-
-    double imax;
-    imax = readInput<double>( "JMAX", SETUP_FILE );
-    cout << "Value from input file: " << imax << "\n";
+    
+    print(m);
 
     return 0;
 }
--- cc-297/src/Schemes/ConvectiveSchemes.C
+++
@@ -1,6 +0,0 @@
-#include "ConvectiveSchemes.H"
-
-void testingSubdirectory()
-{
-    std::cout<< "This is a print function from inside ConvectiveSchemes\n" << std::endl;
-}
